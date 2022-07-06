fc686322ea7105671e2229abaea8a251f176a047
Agrawal05 <deeksha895474@gmail.com>
2022-07-06 19:11:50 +0530

    Initial Commit

---
+++ src/main/java/com/example/StudentManagement/StudentManagementApplication.java
@@ -0,0 +1,16 @@
+package com.example.StudentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+
+@SpringBootApplication
+public class StudentManagementApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+}

d695152d8e9439f59556b9b98bff5c3a8f0cd0d6
MunishPathak <pathak.munish1985@gmail.com>
2022-07-16 18:43:47 +0530

    Added a test comment

--- src/main/java/com/example/StudentManagement/StudentManagementApplication.java
+++ src/main/java/com/example/StudentManagement/StudentManagementApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentManagementApplication{
 
 	public static void main(String[] args) {
+		//Test Comment
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
